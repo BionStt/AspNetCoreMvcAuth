819b6bf5fc0a19cca335c5ee3d31c3902d1845f9
Ibro <ibro@ibro.me>
2017-12-06 09:05:16 +0100

    Add project files.

---
+++ AspNetCoreMvcAuth/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AspNetCoreMvcAuth.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
